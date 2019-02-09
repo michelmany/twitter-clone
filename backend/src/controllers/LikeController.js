532d31dee15d36fad124e5c944f3663ddfd9c779
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2019-02-09 18:32:27 +0100

    Adding Like Controller and Model

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,13 @@
+const Tweet = require("../models/Tweet");
+
+module.exports = {
+  async store(req, res) {
+    const tweet = await Tweet.findById(req.params.id);
+
+    tweet.set({ likes: tweet.likes + 1 });
+
+    await tweet.save();
+
+    return res.json(tweet);
+  }
+};
